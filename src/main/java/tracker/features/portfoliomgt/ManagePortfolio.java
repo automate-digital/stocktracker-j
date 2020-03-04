06d1458c2c077ea4a17cc7a6e6201bf9e3dc1607
Dit <it@automate.digital>
2020-03-04 02:33:04 +0000

    Fix bug when new units added for existing ticker

--- src/main/java/tracker/features/portfoliomgt/ManagePortfolio.java
+++ src/main/java/tracker/features/portfoliomgt/ManagePortfolio.java
@@ -6,6 +6,7 @@ import tracker.service.PricingService;
 import tracker.storage.Persistence;
 
 import java.util.List;
+import java.util.Map;
 
 public class ManagePortfolio {
     PricingService pricingService;
@@ -19,26 +20,19 @@ public class ManagePortfolio {
     }
 
     public void add(String ticker, int units) throws Exception {
+        if (find(ticker)) portfolio.delete(ticker); // duplicates not allowed
         load(ticker, units);
         save(ticker, units);
+    }
+
+    private boolean find(String ticker) {
+        Map<String, Integer> stocks = persistence.getAllStocks();
+        return stocks.containsKey(ticker);
     }
 
     public void load(String ticker, int units) throws Exception {
         Stock stock = getStock(ticker, units);
         portfolio.add(stock);
-    }
-
-    public void delete(String ticker) {
-        portfolio.delete(ticker);
-        remove(ticker);
-    }
-
-    public double getValuation() {
-        return portfolio.getValue();
-    }
-
-    public List<Stock> getListing() {
-        return portfolio.getStocks();
     }
 
     private Stock getStock(String ticker, int units) throws Exception {
@@ -47,14 +41,33 @@ public class ManagePortfolio {
     }
 
     private void save(String ticker, int units) {
-        String sql = "INSERT INTO " + Persistence.STOCKS_TABLE_NAME
-                + " VALUES('" + ticker + "', '" + units + "')";
+        String sql = null;
+        if (!find(ticker)) {
+            sql = "INSERT INTO " + Persistence.STOCKS_TABLE_NAME
+                    + " VALUES('" + ticker + "', '" + units + "')";
+        } else {
+            sql = "UPDATE " + Persistence.STOCKS_TABLE_NAME
+                    + " SET units=" + units + " WHERE ticker='" + ticker + "'";
+        }
         persistence.executeQuery(sql);
+    }
+
+    public void delete(String ticker) {
+        portfolio.delete(ticker);
+        remove(ticker);
     }
 
     private void remove(String ticker) {
         String sql = "DELETE FROM " + Persistence.STOCKS_TABLE_NAME
                 + " WHERE ticker='" + ticker + "'";
         persistence.executeQuery(sql);
+    }
+
+    public double getValuation() {
+        return portfolio.getValue();
+    }
+
+    public List<Stock> getListing() {
+        return portfolio.getStocks();
     }
 }
